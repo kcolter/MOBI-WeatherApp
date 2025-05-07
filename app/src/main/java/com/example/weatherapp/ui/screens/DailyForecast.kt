6a80ef84566e8d07d2f1b9b4ef84561815195af2
kcolter <kcolter3@gmail.com>
2025-05-07 11:22:18 -0300

    Scroll not functioning in DailyForecast

--- app/src/main/java/com/example/weatherapp/ui/screens/DailyForecast.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/DailyForecast.kt
@@ -1,12 +1,10 @@
 package com.example.weatherapp.ui.screens
 
-import android.widget.ScrollView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,8 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.R
 
@@ -43,7 +39,6 @@ fun ForecastedDay(fc: Forecast ){
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
-            .verticalScroll(state = rememberScrollState())
             .background(color = MaterialTheme.colorScheme.background)
             .padding(10.dp)
     ) {
@@ -115,8 +110,10 @@ fun ForecastedDay(fc: Forecast ){
 
 @Composable
 fun ThreeDayForecast(){
-
-    Column {
+    Column (
+        modifier = Modifier
+            .verticalScroll(state = rememberScrollState())
+    ){
         ForecastedDay(SampleForecasts.day1)
         ForecastedDay(SampleForecasts.day2)
         ForecastedDay(SampleForecasts.day3)
