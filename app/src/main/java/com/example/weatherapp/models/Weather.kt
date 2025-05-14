d479db6b76761c7d057297055bf1e8d6df57680f
kcolter <kcolter3@gmail.com>
2025-05-14 11:29:43 -0300

    wrote models for Current, Forecast, and Weather

--- app/src/main/java/com/example/weatherapp/models/Weather.kt
+++ app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -1,2 +1,27 @@
 package com.example.weatherapp.models
 
+data class Current(
+    val condition: String,
+    val temperature: Double,
+    val precipitationType: String,
+    val precipitationAmount: Double,
+    val imagePath: String //potentially unneeded: unknown if a URI will be supplied by API, or if we're going to use local resources based on condition
+    )
+
+data class Forecast(
+    val date: String,
+    val temperatureHigh: Double,
+    val temperatureLow: Double,
+    val condition: String,
+    val precipitationType: String,
+    val precipitationAmount: Double,
+    val precitationProbability: Double,
+    val windDirection: String,
+    val windSpeed: Double,
+    val imagePath: String //potentially unneeded: unknown if a URI will be supplied by API, or if we're going to use local resources based on condition
+    )
+
+data class Weather(
+    val currentWeather: Current,
+    val forecasts: List<Forecast>
+)
