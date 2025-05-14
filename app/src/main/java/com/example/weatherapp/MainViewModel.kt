e593bf747e8ba7553bd99ac4d64ff9ca722b71d9
kcolter <kcolter3@gmail.com>
2025-05-14 12:05:17 -0300

    fixed constructed Current object, still working out way to pass values to Screens

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -15,7 +15,7 @@ class MainViewModel : ViewModel() {
     init {
         //create weather object
         val w = Weather(
-            currentWeather = Current("Raining", 15.0, "Rain", 3.1, "imgpath"),
+            currentWeather = Current("Raining", 15.0, "Rain", 3.1, "North", 12.0, "imgpath"),
             forecasts = listOf(
                 Forecast("May 15, 2025", 15.0, 5.1, "Sunny", "None", 0.0, 0.0, "North", 10.1, "imgpath"),
                 Forecast("May 16, 2025", 16.0, 6.1, "Sunny", "None", 0.0, 0.0, "North", 10.1, "imgpath"),
