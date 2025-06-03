617cc4697c8a6dfd9c6e48e586df04361f67a47a
kcolter <kcolter3@gmail.com>
2025-06-02 21:29:49 -0300

    rewrote model to match the JSON format of the API

--- app/src/main/java/com/example/weatherapp/models/Weather.kt
+++ app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -1,38 +1,48 @@
 package com.example.weatherapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Current(
     val condition: Condition,
-    val temp_c: Double,
+    @SerializedName("temp_c") val temperature: Double,
     val precipitationType: String, //no precip-type in api, will have to determine based on mm or in not being 0 IF precipitation is present
     val precip_mm: Double,
     val precip_in: Double,
     val wind_dir: String,
     val wind_kph: Double,
-    //val imagePath: String image will by taken from icon in condition
+    //val imagePath: image will by taken from icon in condition
     )
 
-
+//Condition  used in both Current and forecastday objects
 data class Condition(
     val text: String,
-    val icon: String,
+    val icon: String, //the cdn-provided icon, 64x64 png
     val code: Int
 )
 
 data class Forecast(
-    val date: String,
-    val temperatureHigh: Double,
-    val temperatureLow: Double,
-    val condition: String,
-    val precipitationType: String,
-    val precipitationAmount: Double,
-    val precipitationProbability: Double,
-    val windDirection: String,
-    val windSpeed: Double,
-    val humidity: Double,
-    val imagePath: String //potentially unneeded: unknown if a URI will be supplied by API, or if we're going to use local resources based on condition
+    val forecastday: List<Forecastday> //api-response structure forecast->forecastday->0, 1, 2, ...
+)
+
+data class Forecastday(
+    val date: String, // format of YYYY-MM-DD
+    val day: Day,
+    //val imagePath: String //image will by taken from icon in condition found in day
     )
+
+data class Day(
+    val condition: Condition,
+    @SerializedName("maxtemp_c") val temperatureHigh: Double,
+    @SerializedName("mintemp_c") val temperatureLow: Double,
+    @SerializedName("maxwind_kph") val windSpeed: Double,
+    @SerializedName("avghumidity")val humidity: Double,
+    val precipitationType: String,
+    @SerializedName("totalprecip_mm")val precipitationAmount: Double,
+    @SerializedName("daily_chance_of_rain") val precipitationProbability: Double,
+    val windDirection: String,
+)
 
 data class Weather(
     val currentWeather: Current,
-    val forecasts: List<Forecast>
+    val forecast: Forecast //api-response structure forecast->forecastday->0, 1, 2, ...
 )
