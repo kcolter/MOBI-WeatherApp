f59e865947756b8c3894991f01713fdca3f8af35
kcolter <kcolter3@gmail.com>
2025-06-03 18:03:07 -0300

    rewrote MainViewModel to call API after the long. and lat. have been updated

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -15,27 +15,29 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainViewModel : ViewModel() {
 
 
-    fun updateLocation(longAndLatIn: String){
-        _longAndLat = longAndLatIn
-    }
-
     var _longAndLat = ""
 
     private val _weather = MutableStateFlow<Weather?>(null)
     val weather = _weather.asStateFlow()
 
+    //create retrofit instance
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.weatherapi.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    init {
-        //create retrofit instance
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://api.weatherapi.com")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    //init interface
+    val weatherService: WeatherService = retrofit.create(WeatherService::class.java)
 
-        //init interface
-        val weatherService: WeatherService = retrofit.create(WeatherService::class.java)
+    fun updateLocation(longAndLatIn: String){
+        _longAndLat = longAndLatIn
+        callAPI() //view models .launch located in here so we know Long. and Lat. are updated
+    }
 
-        //get data from API
+    fun callAPI(){
+        if(_longAndLat.isBlank()) return
+
+        //get data from API using updated longitude and latitude
         viewModelScope.launch {
             val w = weatherService.getForecast("d8e81af2b0f14539ba5143224252105", _longAndLat, 3, "no", "no") //retrieve from api
 
